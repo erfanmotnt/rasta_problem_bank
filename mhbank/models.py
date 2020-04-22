63bd7a76d52c2b5c5c9adad5d3ae58adbdb3d3c2
erfanmotnt <moeini.erfan@gmail.com>
2020-04-22 21:31:31 +0430

    edit models and migrate

--- mhbank/models.py
+++ mhbank/models.py
@@ -1,35 +1,6 @@
 from django.db import models
 
 
-class Question(models.Model):
-    name = models.CharField(max_length=200)
-    level = models.IntegerField()
-    verification_status = models.CharField(max_length=50)
-    appropriate_grades_min = models.IntegerField(default=1, max_length=2)
-    appropriate_grades_max = models.IntegerField(default=12, max_length=2)
-    tags = models.ManyToManyField(Tag)
-    events = models.ManyToManyField(Event)
-    source = models.ForeignKey(Source, on_delete=models.CASCADE)
-    question_maker = models.ForeignKey(Account, on_delete=models.CASCADE)
-    text = models.CharField()
-    answer = models.CharField()
-    published_date = models.DateTimeField('date published')
-
-    #themed_qs
-    #emoj
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE) #meny to one
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-    pub_date = models.DateTimeField('date published')
-    publications = models.ManyToManyField(Publication) #meny to meny
-    models.OneToOneField(
-        Place,
-        on_delete=models.CASCADE,
-        primary_key=True,
-    ) # one to one
 
 class Account(models.Model):
     Username = models.CharField(max_length=200)
@@ -43,34 +14,55 @@ class Account(models.Model):
     role = models.CharField(max_length=1)
     #image_url ... not complete
 
+
+class Source(models.Model):
+    name = models.CharField(max_length=200)
+    #questions
+
+
 class Tag(models.Model):
     name = models.CharField(max_length=200)
     #sub_tags
+    
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    #questions
+
+class Question(models.Model):
+    name = models.CharField(max_length=200)
+    level = models.IntegerField()
+    verification_status = models.CharField(max_length=50)
+    appropriate_grades_min = models.IntegerField(default=1)
+    appropriate_grades_max = models.IntegerField(default=12)
+    tags = models.ManyToManyField(Tag)
+    events = models.ManyToManyField(Event)
+    source = models.ForeignKey(Source, on_delete=models.CASCADE)
+    question_maker = models.ForeignKey(Account, on_delete=models.CASCADE)
+    text = models.CharField(max_length=3000)
+    answer = models.CharField(max_length=3000)
+    published_date = models.DateTimeField('date published')
+
+    #themed_qs
+    #emoj
+
 
 class Sub_tag(models.Model):
     name = models.CharField(max_length=200)
     parent = models.ForeignKey(Tag, on_delete=models.CASCADE)
 
 
-class Event(models.Model):
-    name = models.CharField(max_length=200)
-    #questions
-
-class Source(models.Model):
-    name = models.CharField(max_length=200)
-    #questions
 
 class Attempt(models.Model):
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     time = models.IntegerField(default=0)
     date = models.DateTimeField()
-    attempt = models.ForeignKey(Attempt, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
 
 
     
 class Themed_q(models.Model):
     name = models.CharField(max_length=200)
-    text = models.CharField()
+    text = models.CharField(max_length=3000)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     
