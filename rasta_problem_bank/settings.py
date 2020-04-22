098ecd78067a71f8c4a5ad28e6a1bf7402aefa64
erfanmotnt <moeini.erfan@gmail.com>
2020-04-22 18:41:13 +0430

    initialize

--- rasta_problem_bank/settings.py
+++ rasta_problem_bank/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'mhbank.apps.PollsConfig',
+    'mhbank.apps.MhbankConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
