effbe50d98dbb7170181367b027e0ac39175aa1e
erfanmotnt <moeini.erfan@gmail.com>
2020-04-23 17:08:20 +0430

    change q desine

--- mhbank/admin.py
+++ mhbank/admin.py
@@ -5,8 +5,16 @@ from .models import Question, Account, Tag, Sub_tag
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
+    fields = ['name', 'level', 'verification_status', 'text', 'answer', 'source', 'events',
+     'appropriate_grades_min', 'appropriate_grades_max', 'tags', 'sub_tags', 'question_maker', 'last_change_date']
+    
     readonly_fields = ['question_maker', 'last_change_date']
+    
+    list_display = ('name', 'question_maker', 'last_change_date')
 
+    #events only for admin ...
+    #vertify only for mentors and admin
+    #all defualt saved for each users
     def save_model(self, request, obj, form, change):
         if not change:
             obj.question_maker = request.user.account
@@ -41,6 +49,11 @@ class SubTagInline(admin.TabularInline): #StackedInline
 
 class TagAdmin(admin.ModelAdmin):
     inlines = [SubTagInline]
+    
+class Sub_tagAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return True
 
 
 admin.site.register(Tag, TagAdmin)
+admin.site.register(Sub_tag, Sub_tagAdmin)
