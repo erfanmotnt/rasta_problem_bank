0370615e4f786d7fbef0aa294e5e6b073702957f
Erfan Loghmani <Elkiscoming@gmail.com>
2020-04-30 21:08:28 +0430

    Fix entrypoint problem & change dockerfile

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-pipenv shell
+
 if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
@@ -11,7 +11,7 @@ then
     echo "PostgreSQL started"
 fi
 
-python manage.py migrate
-python manage.py collectstatic --no-input --clear
+pipenv run python manage.py migrate
+pipenv run python manage.py collectstatic --no-input --clear
 
 exec "$@"
