3e525beeafa9ad6264e62dceca8ef033479b83c3
erfanmotnt <moeini.erfan@gmail.com>
2020-05-20 19:19:52 +0430

    edit3

--- mhbank/models.py
+++ mhbank/models.py
@@ -67,7 +67,7 @@ class Question(models.Model):
     answer = models.CharField(max_length=3000, null=True, blank=True)
     #guidance = models.CharField(max_length=1000)
     publish_date = models.DateTimeField('date published')
-    #change_date = models.DateTimeField('date changed', default=timezone.localtime())
+    change_date = models.DateTimeField('date changed', null=True)
     # themed_qs
     # emoj
 
