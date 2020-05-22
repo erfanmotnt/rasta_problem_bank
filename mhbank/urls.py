6437b87589ad1161fd716bdc696f76b5071fc554
erfanmotnt <moeini.erfan@gmail.com>
2020-05-23 02:29:21 +0430

    question view html

--- mhbank/urls.py
+++ mhbank/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('<int:question_id>/', views.index, name='index'),
 ]
--- mhbank/views.py
+++ mhbank/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404, reverse
+
+from .models import Question
 
 
-def index(request):
-    return render(request, 'question.html')
+def index(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'question.html', {'question': question})
