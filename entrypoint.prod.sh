be3f677c32a8e256125dec8d083f89b78c1a85af
moeini <moeini.erfan@gmail.com>
2020-12-21 23:48:10 +0000

    !

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -11,7 +11,7 @@ then
     echo "PostgreSQL started"
 fi
 
-pipenv run python manage.py migrate
-pipenv run python manage.py collectstatic --no-input --clear
+pipenv --python 3.7.4 run python manage.py migrate
+pipenv --python 3.7.4 run python manage.py collectstatic --no-input --clear
 
 exec "$@"
