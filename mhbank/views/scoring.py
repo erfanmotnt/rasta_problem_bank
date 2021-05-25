3cf037fc54610ee91215c3a721a39111a0c7502c
erfanmotnt <moeini.erfan@gmail.com>
2021-05-25 23:06:02 +0430

    debugg and test qscoring

--- mhbank/views/scoring.py
+++ mhbank/views/scoring.py
@@ -13,12 +13,18 @@ from mhbank.views.permissions import QuestionPermission
 def score_question(request):
     serializer = ScoreQuestionSerializer(data=request.data)
     if not serializer.is_valid(raise_exception=True):
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-    data = serializer.validated_data['question']
+        return Response("invalid data", status=status.HTTP_400_BAD_REQUEST)
+    data = serializer.validated_data
     data['account'] = request.user.account
+    try:
+        qinstance = Question.objects.filter(id=data['question'])[0]
+    except:
+        return Response("invalid question", status=status.HTTP_400_BAD_REQUEST)
+    data['question'] = qinstance
+
     if abs(data['score']) > 1:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-    
+        return Response("wrong score", status=status.HTTP_400_BAD_REQUEST)
+
     try:
         instance = AccountScoreQuestion.objects.filter(question = data['question'],\
                                                         account = data['account'])[0]
@@ -26,13 +32,11 @@ def score_question(request):
     except:
         instance = AccountScoreQuestion.objects.create(**data)
         last_score = 0
-        
+    new_score = max(min(data['score'] + last_score, 1), -1)
+    instance.score = new_score
     instance.save()
-    delta_score = max(min(data['score'] + last_score, 1), -1) - last_score
-    try:
-        instance = Question.filter(id=data['question'])[0]
-    except:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-    instance.score += delta_score
-    instance.save()
-    return Response({'score': instance.score}, status=status.HTTP_200_OK)
+    delta_score =  new_score - last_score
+    qinstance.score += delta_score
+    print(delta_score, qinstance.score, last_score)
+    qinstance.save()
+    return Response({'score': qinstance.score}, status=status.HTTP_200_OK)
